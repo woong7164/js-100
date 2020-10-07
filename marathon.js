79827bfdd8971f50a11cea43d4fd38217dca0044
woong7164 <woong7164@gmail.com>
2020-10-07 20:16:14 +0900

    add solution2

--- marathon.js
+++ marathon.js
@@ -8,3 +8,13 @@ function solution(participant, completion) {
         if(participant[i] !== completion[i]) return participant[i];
     }
 }
+
+function solution2(participant, completion) {
+    var dic = completion.reduce((obj, t)=> (obj[t]= obj[t] ? obj[t]+1 : 1 , obj) ,{});
+    return participant.find(t=> {
+        if(dic[t])
+            dic[t] = dic[t]-1;
+        else 
+            return true;
+    });
+}
