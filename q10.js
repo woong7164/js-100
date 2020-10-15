cfb170f1edbeece2abdb2c9836bf02eebdd4350c
woong7164 <woong7164@gmail.com>
2020-10-15 23:46:50 +0900

    Update q10.js

--- q10.js
+++ q10.js
@@ -27,39 +27,3 @@ solution();
 
 
 
-const map = [
-  {
-    A:"banana",
-    B:"apple",
-    C:"kim",
-    D:"lee"
-  },
-
-  {
-    A:"kim",
-    B:"apple",
-    C:"banana",
-    D:"lee"
-  },
-
-  {
-    A:"apple",
-    B:"banana",
-    C:"kim",
-    D:"lee"
-  }
-
-]
-
-const obj = map.reduce((acc, obj)=>{                    
-  let key = obj["A"];
-  if(!acc[key]){
-    acc[key] = [];
-  }
-  
-  acc[key].push(obj);
-  return acc;
-},{});
-
-console.log(obj)
-
